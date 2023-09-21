809c9d784c2d8732a93ca7cee5b44d1c2a047171
darpan saini <darpansaini1997@gmail.com>
2023-09-21 05:43:46 +0530

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/DarpanSaini100" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
